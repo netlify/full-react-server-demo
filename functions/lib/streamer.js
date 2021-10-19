538f140e912d2b9e0899744584cb4ee0d5b14007
Mathias Biilmann Christensen <info@mathias-biilmann.net>
2021-10-18 18:46:45 -0700

    Fix broken await

--- functions/lib/streamer.js
+++ functions/lib/streamer.js
@@ -89,7 +89,7 @@ export const streamer = (handler) =>
                     headers: headers
                 }))
 
-                await handler(event, writer, context)
+                handler(event, writer, context)
             })
             
         } 
